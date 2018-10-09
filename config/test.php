072118c78e46cdc4d40bb70299762a81ce175f32
askew <askew.39rus@gmail.com>
2018-10-09 13:58:28 +0200

    Add test config, congig db

--- config/test.php
+++ config/test.php
@@ -19,7 +19,7 @@ return [
             'useFileTransport' => true,
         ],
         'assetManager' => [
-            'basePath' => __DIR__ . '/../web/assets',
+            'basePath' => __DIR__ . '/../public/assets',
         ],
         'urlManager' => [
             'showScriptName' => true,
