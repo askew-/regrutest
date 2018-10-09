e10bf8ce4ebf9373015a0cdf8361604b077d9dae
askew <askew.39rus@gmail.com>
2018-10-09 13:57:48 +0200

    Add phpdoc in Company

--- models/Company.php
+++ models/Company.php
@@ -4,10 +4,17 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
+/**
+ * Class Company
+ * @property $id
+ * @property $user_id
+ * @property $title
+ * @package app\models
+ */
 class Company extends ActiveRecord
 {
 
-    public static function tableName()
+    public static function tableName(): string
     {
         return '{{%company}}';
     }
